70637490cec15bb667a9a95891ede0d724d94121
Zakk Fast <zakkfastpro@gmail.com>
2021-09-09 19:27:57 -0700

    removed express

--- main.js
+++ main.js
@@ -17,14 +17,6 @@ const db = mysql.createConnection(
   console.log(`Connected to the employee_tracker_db.`)
 );
 
-app.use((req, res) => {
-  res.status(404).end();
-});
-
-app.listen(PORT, () => {
-  console.log(`Server running on ${PORT}`);
-});
-
 function runPrompt() {
   inquirer
     .prompt([
